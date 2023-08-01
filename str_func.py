3e96fec68da14ea117aa8bdb2add1c62cb837a93
ElenaUst <elka_us@mail.ru>
2023-08-01 23:25:32 +0300

    add docstrings and title_text

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def upper_text(word):
+    """возвращает текст заглавными буквами"""
     return word.upper()
+
+def title_text(word):
+    """возвращает текст c заглавной буквы"""
+    return word.title()
